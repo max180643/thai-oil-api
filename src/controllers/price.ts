8869a2fb1e7577558affe7bc4f20785976dd3250
max180643 <max180643@gmail.com>
2021-12-29 02:41:10 +0700

    :ambulance: update script

--- src/controllers/price.ts
+++ src/controllers/price.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import PRICE_CONFIG from "../config/price";
 
 const priceHandler = async () => {
